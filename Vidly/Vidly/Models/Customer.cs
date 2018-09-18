63a17f1536b1c851b15c656b3d18a8a06585ed1e
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-09-18 13:54:21 +1000

    Changing the model and run update-database, add-migration from Package Manager declare Foreign Key and Navigation property

--- Vidly/Vidly/Models/Customer.cs
+++ Vidly/Vidly/Models/Customer.cs
@@ -9,5 +9,11 @@ namespace Vidly.Models
     {
         public int Id { get; set; }
         public string Name { get; set; }
+        public bool IsSubscribedToNewsletter { get; set; }
+
+        //It's called Navigation Property because it allows us to navigate from one type to another
+        public MembershipType MembershipType { get; set; }
+        //EntityFramework recognizes the foreign key
+        public byte MembershipTypeId { get; set; }
     }
 }
