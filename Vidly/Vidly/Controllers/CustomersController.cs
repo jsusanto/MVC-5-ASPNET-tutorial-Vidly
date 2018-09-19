ce9b8a31b30ae880103c6b4c4642fae6d784a235
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-09-19 17:43:25 +1000

    Querying objects

--- Vidly/Vidly/Controllers/CustomersController.cs
+++ Vidly/Vidly/Controllers/CustomersController.cs
@@ -9,10 +9,27 @@ namespace Vidly.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
         // GET: Customers
         public ActionResult Index()
         {
-            var customers = GetCustomers();
+            //var customers = GetCustomers();
+
+            //when this line's executed, EntityFramework won't query to database and it's called deferred execution
+            //The query gets executed when we iterate the customers object
+            //var customers = _context.Customers;
+
+            //Alternately we can immediately query the query by calling toList()
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
@@ -28,7 +45,7 @@ namespace Vidly.Controllers
 
         public ActionResult Details(int id)
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
