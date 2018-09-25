1217acb948b63f8fe7abb81833608d73956398c4
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-09-25 10:18:25 +1000

    Create the Web API for Movies

--- Vidly/Vidly/App_Start/MappingProfile.cs
+++ Vidly/Vidly/App_Start/MappingProfile.cs
@@ -11,8 +11,20 @@ namespace Vidly.App_Start
     public class MappingProfile : Profile
     {
         public MappingProfile() {
+            // Domain to Dto
+            Mapper.CreateMap<Movie, MovieDto>();
             Mapper.CreateMap<Customer, CustomerDto>();
-            Mapper.CreateMap<CustomerDto, Customer>();
+
+            // Dto to Domain
+            /*
+             Id is the key property for the Movie class, and a key property should not be changed. 
+             That’s why we get this exception. To resolve this, you need to tell AutoMapper to ignore Id during mapping of a MovieDto to Movie. 
+             */
+            Mapper.CreateMap<CustomerDto, Customer>().
+                ForMember(c => c.Id, opt => opt.Ignore()); ;
+            Mapper.CreateMap<MovieDto, Movie>().
+                ForMember(m => m.Id, opt =>  opt.Ignore());
+
         }
     }
 }
---
+++ Vidly/Vidly/Controllers/Api/MoviesController.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Models;
+using Vidly.Dtos;
+using AutoMapper;
+
+namespace Vidly.Controllers.Api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public MoviesController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        // api/customers
+        public IEnumerable<MovieDto> GetMovies()
+        {
+            return _context.Movies.ToList().Select(Mapper.Map<Movie, MovieDto>);
+        }
+
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movie == null)
+                return NotFound();
+
+            return Ok(Mapper.Map<Movie, MovieDto>(movie));
+        }
+
+        // POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            var _movie = Mapper.Map<MovieDto, Movie>(movieDto);
+
+            _context.Movies.Add(_movie);
+            _context.SaveChanges();
+
+            return Created(new Uri(Request.RequestUri + "/" + _movie.Id), movieDto);
+        }
+
+        // PUT /api/movies/1
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, MovieDto movieDto)
+        {
+            if (!ModelState.IsValid)
+            {
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            }
+
+            var movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map<MovieDto, Movie>(movieDto, movieInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        } 
+
+        // DELETE /api/movies
+        [HttpDelete]
+        public IHttpActionResult DeleteMovie(int id)
+        {
+            var _movieInDb = _context.Movies.SingleOrDefault(m => m.Id == id);
+
+            if (_movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Movies.Remove(_movieInDb);
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
