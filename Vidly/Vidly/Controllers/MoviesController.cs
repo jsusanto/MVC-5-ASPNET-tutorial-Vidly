32ed758efb591c0080fc6433a72a4be76b193ae8
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-09-27 15:29:59 +1000

    Add authorisation on edit

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -112,6 +112,7 @@ namespace Vidly.Controllers
             return View("MovieForm", viewModel);
         }
 
+        [Authorize(Roles = RoleName.CanManageMovies)]
         public ActionResult Edit(int id)
         {
             var movie = _context.Movies.SingleOrDefault(m => m.Id == id);
