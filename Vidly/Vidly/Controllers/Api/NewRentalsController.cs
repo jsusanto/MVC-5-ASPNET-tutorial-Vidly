11730cb340fffa37fc886eac02b44f6b0b8c3740
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-09-29 16:08:44 +1000

    Add new field - Number Available on Movie Table, Create the rental class

---
+++ Vidly/Vidly/Controllers/Api/NewRentalsController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Vidly.Dtos;
+using Vidly.Models;
+
+namespace Vidly.Controllers.Api
+{
+    public class NewRentalsController : ApiController
+    {
+        private ApplicationDbContext _context;
+
+        public NewRentalsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
+        [HttpPost]
+        public IHttpActionResult CreateNewRentals(NewRentalDto newRental)
+        {
+            /* 
+             * using .Single because we assume that the customer will be sending the right customer id from drop down list
+             * if we're building a public API that can be used by various different application, we should us SingleOrDefault
+             * 
+             * var customer = _context.Customers.SingleOrDefault(c => c.Id == newRental.CustomerId);
+             * if (customer == null)
+             *   return BadRequest("Invalid Customer ID");
+             */
+
+            var customer = _context.Customers.Single(
+                c => c.Id == newRental.CustomerId);
+
+            /* 
+             * To load multiple movies
+             * The following code is the translation of 
+             * SELECT * FROM Movies WHERE Id in (1,2,3); 
+             */
+            var movies = _context.Movies.Where
+                (m => newRental.MovieIds.Contains(m.Id));
+
+            foreach(var movie in movies){
+                movie.NumberAvailable--;
+                var rental = new Rental
+                {
+                    Customer = customer,
+                    Movie = movie,
+                    DateRented = DateTime.Now
+                };
+                _context.Rentals.Add(rental);
+            }
+
+            _context.SaveChanges();
+
+            return Ok();
+        }
+    }
+}
