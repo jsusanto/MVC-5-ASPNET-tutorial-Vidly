07bd70ecf2e96a637070067cc34cfefe217d4b98
Jeffry Susanto <jeffry.susanto@hotmail.com>
2018-10-06 00:47:16 +1000

    Add the filter on Movie API- new rental page

--- Vidly/Vidly/Controllers/Api/MoviesController.cs
+++ Vidly/Vidly/Controllers/Api/MoviesController.cs
@@ -26,12 +26,30 @@ namespace Vidly.Controllers.Api
         }
 
         // api/customers
+        /*
         public IEnumerable<MovieDto> GetMovies()
         {
             return _context.Movies
                    .Include(m => m.Genre)  //using System.Data.Entity;
                    .ToList()
                    .Select(Mapper.Map<Movie, MovieDto>);
+        }*/
+        
+        public IEnumerable<MovieDto> GetMovies(string query)
+        {
+            var moviesQuery = _context.Movies
+                              .Include(m => m.Genre)
+                              .Where(m => m.NumberAvailable > 0);
+
+            if (!String.IsNullOrWhiteSpace(query))
+            {
+                moviesQuery = moviesQuery.Where(m => m.Name.Contains(query));
+            }
+
+            var moviesDto = moviesQuery.ToList()
+                            .Select(Mapper.Map<Movie, MovieDto>);
+
+            return moviesDto;
         }
 
         public IHttpActionResult GetMovie(int id)
